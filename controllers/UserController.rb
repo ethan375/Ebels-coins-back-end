995aa114cf142203fe3aa281e51f0d34bb59b388
ethan375 <ethanebel1@gmail.com>
2018-03-05 18:42:13 -0600

    user login/out functionality added

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -1,6 +1,36 @@
 class UserController < ApplicationController
 
-  get '/' do 
-    "just some placeholder things for rn while I get sessions in order"
+  post '/register' do 
+    @user = User.new
+    @username = params[:username]
+    @password = params[:password]
+    @user.save
+    session[:logged_in] = true
+    session[:user_id] = @user.id
+    session[:username] = @user.username
+    session[:message] = "You are logged in"
+    resp = {
+      message: session[:message]
+    }.to_json
+  end 
+
+  post '/login' do
+    @password = params[:password]
+    @user = User.find_by(username: params[:username])
+    success = nil
+    if @user && @user.authenticate(@password)
+      success = true
+      session[:username] = @user.username
+      session[:logged_in] = true
+      session[:user_id] = @user.id
+      session[:message] = "Logged in as #{@user.username}"
+    else 
+      success = false
+      session[:message] = "Invalid username or password"
+    end
+    resp = {
+      message: session[:message],
+      success: success
+    }.to_json
   end 
 end 
